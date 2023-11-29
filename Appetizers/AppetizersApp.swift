2982b651084c6a151488df879f96960ad3e3de25
shibu92rk <shibu92rk@gmail.com>
2023-11-29 08:00:58 -0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ramya Adattiri on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
